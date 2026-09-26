61fe26ad9b8a375ea6eab9baed430fe172a90cf6
sDANTOSO <winatsa@whs.school.nz>
2026-09-26 13:18:08 +1200

    added larger background

--- fruity-space/Scripts/start.gd
+++ fruity-space/Scripts/start.gd
@@ -1,5 +1,6 @@
 extends Control
 @onready var planet_v_2: Sprite2D = $PlanetV2
+@onready var camera_2d: Camera2D = $Camera2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,3 +12,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	planet_v_2.rotation= planet_v_2.rotation+0.0005
 	
+
+
+func _on_play_button_pressed() -> void:
+	
+	var tween = create_tween()
+	zoom_out()
+	tween.tween_property(camera_2d, "position", Vector2(600, 1337), 4.0).set_trans(Tween.TRANS_CUBIC)
+	##tween.tween_property(camera_2d, "zoom", Vector2(0.35, 0.35), 4.0)
+	
+func zoom_out() -> void:
+	var tween = create_tween()
+	tween.tween_property(camera_2d, "zoom", Vector2(0.35, 0.35), 4.0).set_trans(Tween.TRANS_CUBIC)
