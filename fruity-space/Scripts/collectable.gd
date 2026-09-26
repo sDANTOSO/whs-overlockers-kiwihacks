57fecf21f8648cefeb99191e6829c13398f64bde
James <howarja@whs.school.nz>
2026-09-26 12:41:12 +1200

    collectables win detection

--- fruity-space/Scripts/collectable.gd
+++ fruity-space/Scripts/collectable.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+var collected: bool = false;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Globals.new_collectable();
 	self.body_entered.connect(collision);
 
 func collision(body: Node2D):
@@ -10,11 +13,14 @@ func collision(body: Node2D):
 		collect_anim();
 
 func collect_anim():
-	print("Collected!");
-	$AnimationPlayer.play("Collect");
+	if !collected:
+		collected = true;
+		print("Collected!");
+		$AnimationPlayer.play("Collect");
+		# increase global amount collected here
+		Globals.reduce_collectables();
+
 
 func collect_apply():
-	# increase global amount collected here
-	
 	# destroy the collectable
 	queue_free();
